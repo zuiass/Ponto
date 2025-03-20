d3a151533f667c8304596cbc9a06485073613ca5
Josué <josuealexandre067@gmail.com>
2025-03-20 17:11:32 -0400

    Update index.js

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -9,7 +10,7 @@ function createWindow() {
       nodeIntegration: true
     }
   });
-
+  
   win.loadFile('../renderer/pages/login.html');
 }
 
