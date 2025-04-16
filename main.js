1b26126e86e15b4ebdb9f05c5126d65f56c6762b
Josué <josuealexandre067@gmail.com>
2025-04-16 13:05:07 -0400

    Starter

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true,
+        icon: path.join(__dirname, './src/assets/ponto.png'),
+        webPreferences: {
+            preload: path.join(__dirname, './preload.js'),
+            contextIsolation: true,
+            enableRemoteModule: false,
+            nodeIntegration: false,
+        }
+    });
+
+    win.loadFile('./src/pages/home.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './src/pages/**/*.html',
+    './src/components/**/*.html',
+    './src/**/*.js'
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+}
