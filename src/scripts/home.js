20a0f6ccb745f5cfb11d1d67c54b5a908eb6c65e
Josué <josuealexandre067@gmail.com>
2025-04-26 18:51:49 -0400

    animation

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -31,3 +31,17 @@ document.addEventListener('DOMContentLoaded', () => {
         loginModal.open();
     });
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Seleciona todos os botões com a classe "game-button"
+    const botoes = document.querySelectorAll('.game-button');
+
+    botoes.forEach((botao, index) => {
+        setTimeout(() => {
+            // Remove as classes de invisibilidade e tamanho grande
+            botao.classList.remove('opacity-0', 'scale-125');
+            // Adiciona as classes que deixam o botão visível e no tamanho correto
+            botao.classList.add('opacity-100', 'scale-100');
+        }, 100 + (index * 150)); // Delay entre os botões para a animação em cascata
+    });
+});
