572d48891c70251e0edf68c575a121ccf5148fef
Josué <josuealexandre067@gmail.com>
2025-04-30 15:30:36 -0400

    ...

--- src/components/profileModal.js
+++ src/components/profileModal.js
@@ -22,9 +22,7 @@ export function createProfile() {
                     <img class="h-24 w-24 rounded-full object-cover border-2 border-white/20" src="./../assets/public/fotoTeste.jpg">
 
                     <button class="absolute bottom-0 right-0 bg-yellow-500 rounded-full p-1 hover:bg-yellow-600 transition-colors">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-slate-900">
-                            <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"></path>
-                        </svg>
+                        <img src="../assets/public/addPhoto.svg" />
                     </button>
                 </div>
                 
