b020e05e0507d414a6eef61716dc10734dad5ba8
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-05-08 16:36:29 -0400

    banco novo

--- database/schema.sql
+++ database/schema.sql
@@ -1,29 +1,29 @@
-CREATE DATABASE ponto;
+-- IF NOT EXISTS no SQL é usada para criar um banco de dados ou uma tabela apenas se eles ainda não existirem no sistema. Ela evita erros que aconteceriam se você tentasse criar algo que já existe.
+
+CREATE DATABASE IF NOT EXISTS ponto;
 USE ponto;
 
-CREATE TABLE usuario (
+CREATE TABLE IF NOT EXISTS usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(40) NOT NULL,
     senha VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     pontuacao INT DEFAULT 0
 );
- 
-CREATE TABLE ranking (
+
+CREATE TABLE IF NOT EXISTS ranking (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
     posicao INT NOT NULL,
     pontuacao_total INT NOT NULL,
-    data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+    data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE
 );
- 
-CREATE TABLE historico (
+
+CREATE TABLE IF NOT EXISTS historico (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
-    partida_id INT NOT NULL,
     acao VARCHAR(100) NOT NULL,
     data_acao DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuario(id),
-    FOREIGN KEY (partida_id) REFERENCES partida(id)
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE
 );
