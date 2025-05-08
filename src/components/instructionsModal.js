ce0c7d22396471eaad94eb03459b28df09d14b03
____x_ <josuealexandre067@gmail.com>
2025-05-08 12:32:43 -0400

    new

--- src/components/instructionsModal.js
+++ src/components/instructionsModal.js
@@ -1,5 +1,7 @@
 import { createButton } from '../components/button.js';
 
+// F U N C T I O N S
+
 export function createInstructions() {
     const modal = document.createElement('div');
     modal.className = `
@@ -16,7 +18,7 @@ export function createInstructions() {
 
     modalBox.innerHTML = `
         <div class="flex justify-between items-center mb-6">
-            <h2 class="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-indigo-500">
+            <h2 class="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-yellow-500">
                 Como jogar?
             </h2>
         </div>
@@ -45,17 +47,29 @@ export function createInstructions() {
             </div>
         </div>
 
-        <button id="startPlaying" class="w-full bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 text-white font-medium text-lg py-3 px-4 rounded-xl mt-6 transition-all transform shadow-lg">
-            Entendi!
-        </button>
+        <div class="button-container w-full h-auto">
+            <!-- Entendi! -->
+        </div>
     `;
 
+    const buttonContainer = modalBox.querySelector('.button-container');
+
+    const buttonEntendi = createButton({
+        id: 'startPlaying',
+        text: 'Entendi!',
+        className: 'w-full bg-gradient-to-r from-orange-500 to-yellow-600 hover:from-blue-700 hover:to-indigo-700 text-white font-medium text-lg py-3 px-4 rounded-xl mt-6 transition-all transform shadow-lg',
+        type: 'button'
+    });
+
+    buttonContainer.appendChild(buttonEntendi);
     document.body.appendChild(modal);
     modal.appendChild(modalBox);
 
     modalBox.querySelector('#startPlaying').addEventListener('click', () => {
         close();
     });
+
+    // A N I M A T I O N S
 
     function open() {
         modal.classList.remove('hidden');
@@ -66,6 +80,7 @@ export function createInstructions() {
     }
 
     function close() {
+        void modalBox.offsetWidth;
         modal.classList.remove('opacity-100');
         modal.classList.add('opacity-0');
         modalBox.classList.remove('scale-100', 'opacity-100');
@@ -82,12 +97,10 @@ export function createInstructions() {
     };
 }
 
-// E V E N T S
+// E V E N T S  /  U S I N G
 
 window.addEventListener('DOMContentLoaded', () => {
-    
     const instructionsModal = createInstructions();
-
     const hintButton = document.querySelector('.hint-button');
 
     if (hintButton) {
@@ -95,5 +108,4 @@ window.addEventListener('DOMContentLoaded', () => {
             instructionsModal.open();
         });
     }
-
 });
