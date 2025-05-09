68b2dee3ea76aa562313c94d467183e8de94847c
Josué <josuealexandre067@gmail.com>
2025-05-09 16:22:56 -0400

    Revert "Delete temporaryUsers.js"
    This reverts commit b6163a64bf0cf3c39156e151e69bba612d96906d.

---
+++ database/temporaryUsers.js
@@ -0,0 +1,19 @@
+const usuarios = [
+    {
+        id: 1,
+        apelido: "xX_Kaykera_Xx",
+        email: "kayke@gmail.com",
+        senha: "senha123",
+        foto_url: "../src/assets/public/fotoTeste.jpg",
+        pontos: 150
+    },
+
+    {
+        id: 2,
+        apelido: "____x_",
+        email: "josuias@gmail.com",
+        senha: "outrasenha123",
+        foto_url: "../src/assets/public/fotoTeste2.jpg",
+        pontos: 270
+    }
+];  
